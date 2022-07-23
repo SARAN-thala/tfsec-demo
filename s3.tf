ff7482c0095ae111538687af72de75fcce4226b0
Saran <saranrajmalar@gmail.com>
2022-07-23 13:30:32 +0530

    add tfsec pr commenter in workflow

--- s3.tf
+++ s3.tf
@@ -1,14 +1,14 @@
-resource "aws_s3_bucket" "sample_bucket" {
+resource "aws_s3_bucket" "dummy_bucket" {
     bucket = "my-bucket"
 }
 
 resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.sample_bucket.id
+  bucket = aws_s3_bucket.dummy_bucket.id
   acl = "authenticated-read"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrypt_config" {
-  bucket = aws_s3_bucket.sample_bucket.id
+  bucket = aws_s3_bucket.dummy_bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -17,13 +17,13 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrypt_co
 }
 
 resource "aws_s3_bucket_logging" "bucket_logs" {
-  bucket        = aws_s3_bucket.sample_bucket.id
+  bucket        = aws_s3_bucket.dummy_bucket.id
   target_bucket = "logging-bucket"
   target_prefix = "s3-"
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.sample_bucket.id
+  bucket = aws_s3_bucket.dummy_bucket.id
   versioning_configuration {
     status = "enabled"
   }
