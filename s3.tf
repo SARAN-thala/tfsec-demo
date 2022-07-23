8e3d0ac4000352cc461cf5fe99d4542e9058ec4b
Saran <saranrajmalar@gmail.com>
2022-07-23 12:28:26 +0530

    resolve deprecated params

--- s3.tf
+++ s3.tf
@@ -1,21 +1,30 @@
-resource "aws_s3_bucket" "sampleBucket" {
+resource "aws_s3_bucket" "sample_bucket" {
     bucket = "my-bucket"
+}
 
-    acl = "authenticated-read"
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.sample_bucket.id
+  acl = "authenticated-read"
+}
 
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encrypt_config" {
+  bucket = aws_s3_bucket.sample_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
+}
 
-    logging {
-      target_bucket = "logging-bucket"
-    }
+resource "aws_s3_bucket_logging" "bucket_logs" {
+  bucket        = aws_s3_bucket.sample_bucket.id
+  target_bucket = "logging-bucket"
+  target_prefix = "s3-"
+}
 
-    versioning {
-      enabled = true
-    }
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.sample_bucket.id
+  versioning_configuration {
+    status = "enabled"
+  }
 }
